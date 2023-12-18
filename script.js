205dda58de59addc77e8f5391b039a219c3a5125
Сергій Заморило <zamorylo@i.ua>
2023-12-18 19:00:24 +0200

    .

--- script.js
+++ script.js
@@ -3,28 +3,62 @@ let q=""
 let page=1
 const url="https://pixabay.com/api/"
 let per_page=20
-const ImgContainer=document.querySelector("#ImgCont")
+const ImgContainer=document.querySelector(".ImgCont")
 const Search={
     input: document.querySelector(".input"),
     button: document.querySelector(".btn")
+}
+const favoritecont=document.querySelector(".FavoritreContainer")
+const selector=document.querySelector("#select")
+selector.addEventListener("change",()=>{
+    per_page=selector.value
+})
+let arrIMG=[]
+const RenderFavImages=()=>{
+    favoritecont.innerHTML=`
+    `
+    arrIMG.forEach(Favitem => {
+        favoritecont.innerHTML+=`
+            <div class="favoriteDiv">
+                <img class="FavImageItem" src= "${Favitem}"alt= "${Favitem.tags}">
+            </div>
+    `
+    });
+}
+const pages={
+    input:document.querySelector(".PageInput"),
+    btn:document.querySelector(".PageBtn")
 }
 const RenderImgs=(ImgArr)=>{
     ImgContainer.innerHTML=`
     `
     ImgArr.forEach(item => {
-        ImgContainer.innerHTML=`
-        <img class="ImageItem" src:${item.largeImageURL} alt:${item.tags}>
+        ImgContainer.innerHTML+=`
+            <div class="imageDiv">
+                <img class="ImageItem" src= "${item.largeImageURL}"alt= "${item.tags}">
+                <button class="favbutton" imgUrl="${item.largeImageURL}">Add to favorite</button>
+            </div>
     `
     });
+    const FavBat=document.querySelectorAll(".favbutton")
+    FavBat.forEach(arr=>{
+        arr.addEventListener("click",()=>{
+            const URL=arr.getAttribute("imgUrl")
+            arrIMG.push(URL)
+            console.log(arrIMG)
+            RenderFavImages()
+
+        })    
+    })  
 }
+const DivFavorite=document.querySelector(".favoriteDiv")
 const createGet=()=>{
-    axios.get(`${url}?key=${imgKey}&q=${q}&page=${page}&per_page=${per_page}`
-    .then(res=>{
-        console.log(res.data.hits)
+    axios.get(`${url}?key=${imgKey}&q=${q}&page=${page}&per_page=${per_page}`)
+        .then(res=>{
+        RenderImgs(res.data.hits)
     })
-    )
 }
-Search.input.addEventListener("imput",(el)=>{
+Search.input.addEventListener("input",(el)=>{
     q=el.target.value
 }
 )
@@ -32,4 +66,11 @@ Search.button.addEventListener("click",()=>{
     if (q.length!==0){
         createGet()
     }
+})
+pages.btn.addEventListener("click",()=>{
+    page=pages.input.value
+    axios.get(`${url}?key=${imgKey}&q=${q}&page=${page}&per_page=${per_page}`)
+        .then(resultat=>{
+            RenderImgs(resultat.data.hits)
+    })
 })
